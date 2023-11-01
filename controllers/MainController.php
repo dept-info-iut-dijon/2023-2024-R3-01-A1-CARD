61d04edc0a1740466619b7c24a9ebf2908841069
Romain Card <romain.card9@gmail.com>
2023-11-01 16:28:50 +0100

    TP5 - 3.3 : Essai de routeur pour afficher l'index

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -1,7 +1,7 @@
 <?php
 namespace controllers;
 
-require_once("../views/View.php");
+require_once("views/View.php");
 
 use views\View;
 
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+require_once('controllers/MainController.php');
+
+use controllers\MainController;
+
+// Affichage de l'index
+$controller = new MainController();
+$controller->Index();
