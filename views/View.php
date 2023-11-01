02115dcfb2fab78df51502a1bd5d0bd9496291e6
Romain Card <romain.card9@gmail.com>
2023-11-01 16:00:43 +0100

    TP5 - 2.2 : Implémentation de la classe View

--- views/View.php
+++ views/View.php
@@ -17,7 +17,9 @@ class View
      */
     public function __construct(string $action)
     {
-
+        // Détermination du nom du fichier vue à partir de l'action
+        $this->fichier = "views/vue" . $action . ".php";
+        $this->titre = $action;
     }
 
     /**
@@ -27,17 +29,36 @@ class View
      */
     public function generer(array $donnees): void
     {
-
+        // Génération de la partie spécifique de la vue
+        $contenu = $this->genererFichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererFichier('views/gabarit.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
     }
 
     /**
      * Génère un fichier de vue et renvoie le résultat produit
      * @param string $fichier Fichier à générer
      * @param array $donnees Données nécessaires à la vue
-     * @return void
+     * @return string Résultat de la génération de la vue
      */
-    private function genererFichier(string $fichier, array $donnees)
+    private function genererFichier(string $fichier, array $donnees): string
     {
-
+        if (file_exists($fichier)) {
+            // Rend les éléments du tableau $donnees accessibles dans la vue
+            // Voir la documentation de extract
+            extract($donnees);
+            // Démarrage de la temporisation de sortie
+            ob_start();
+            // Inclut le fichier vue
+            // Son résultat est placé dans le tampon de sortie
+            require $fichier;
+            // Arrêt de la temporisation et renvoi du tampon de sortie
+            return ob_get_clean();
+        }
+        else {
+            throw new Exception("Fichier '$fichier' introuvable");
+        }
     }
 }
